8c9a9dfa21127a17f7386a9c41f11a7285c8b019
abhi-swami <manjuswami338@gmail.com>
2022-12-16 17:07:47 +0530

    updated work

---
+++ December/3(b).todo-using-use-effect-and-crud-operation/src/Components/AddTodo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const AddTodo=()=>{
+    return(
+        <div>
+            Add To Do
+        </div>
+    )
+}
+export default AddTodo
---
+++ December/3(b).todo-using-use-effect-and-crud-operation/src/Components/Todo.jsx
@@ -0,0 +1,7 @@
+export default function Todo(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
